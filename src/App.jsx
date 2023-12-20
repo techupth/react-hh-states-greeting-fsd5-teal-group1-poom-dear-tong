549f752f5bd5e86747d295c98dc76c67b1573e0c
BBTC CTBB <enterpannet@gmail.com>
2023-12-20 21:59:43 +0700

    HH : States

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("Greeting Message");
+  const language = (e) => {
+    setMessage(e.target.value);
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button value="สวัสดี!" onClick={language}>
+          สวัสดี!
+        </button>
+        <button value="Hi!" onClick={language}>
+          Hi!
+        </button>
+        <button value="你好!" onClick={language}>
+          你好!
+        </button>
       </div>
     </div>
   );
