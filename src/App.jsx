6254f044e0d164ecc1dc8d70fff8d3ccdbacde24
Kittipoom Puttaruksa <kittipoom@Kittipooms-MacBook-Pro.local>
2023-12-21 14:09:37 +0700

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [changeLanguage, setChangeLanguage] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{changeLanguage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={(event) => setChangeLanguage("สวัสดี!")}>
+          สวัสดี!
+        </button>
+        <button onClick={(event) => setChangeLanguage("Hi!")}>Hi!</button>
+        <button onClick={(event) => setChangeLanguage("你好!")}>你好!</button>
       </div>
     </div>
   );
