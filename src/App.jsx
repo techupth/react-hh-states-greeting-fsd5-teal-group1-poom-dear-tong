0793455120cbb18530c9589c1226b195bd50e06e
mygit <vuttipat.rungrojbribun@gmail>
2023-12-21 14:09:08 +0700

    Assignment:Dear

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  function GreetingMessage(event) {
+    setDefaultMessage(event.target.value);
+  }
+  const [defaultMessage, setDefaultMessage] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{defaultMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button value={"สวัสดี!"} onClick={GreetingMessage}>
+          สวัสดี!
+        </button>
+        <button value={"Hi!"} onClick={GreetingMessage}>
+          Hi!
+        </button>
+        <button value={"你好!"} onClick={GreetingMessage}>
+          你好!
+        </button>
       </div>
     </div>
   );
